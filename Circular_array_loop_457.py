8040fc7b56bc4aab82ab9969aebd27b8fefb78f7
chitty12 <cty12angel@naver.com>
2024-04-22 01:28:46 +0900

    circular array

---
+++ Circular_array_loop_457.py
@@ -0,0 +1,8 @@
+# 처음 시작하는 인덱스번호 = t
+# 순회 i
+# 숫자 추출 => i + k = index
+# length <= i+k => i+k - length
+# i + k < 0 => length - (k - i)
+# if t = i 가 되는 때 true
+# 다음 인덱스가 i 일때 false
+
--- linked_list.py
+++ linked_list.py
@@ -12,43 +12,44 @@ class ListNode :
     def __init__(self, val = 0, next = None) :
         self.val = val
         self.next = next
-    
-    def LinkedList (self, headList):
+        
+    def settingNode(self, headList):
         head = ListNode(headList[0])
 
-        for node in headList[1:]:
-            head.next = ListNode(node)
-            head = head.next
+        current = head
+        for val in headList[1:]:
+            current.next = ListNode(val)
+            current = current.next
+
         return head
-            
+    
 
 
 class Solution :
     # Optional : typing 모듈에서 제공하는 타입, None 또는 Listnode값을 가짐
     def getComponents(self, head: Optional[ListNode], nums):
-        
         count = 0
         # for는 'iterable'을 순회하므로 적절하지 않음.
         # while은 연속된 노드를 하나씩 순회하면서 확인함.
         while head:
             if head.val in nums:
-                if head.next and head.next in nums:
+                if head.next is None or head.next.val not in nums:                    
                     count += 1
             head = head.next
-
 
         return count
 
 
 
-head = [0, 1, 2, 3]
-nums = [0, 1, 3]
+head_list = [0, 1, 2, 3, 4]
+nums = [0, 3, 1, 4]
 
-node_list = ListNode()
+
+head = ListNode()
+nodeList = head.settingNode(head_list)
 solution = Solution()
-
-head = node_list.LinkedList(head)
-result = solution.getComponents(head, nums)
+print(nodeList)
+result = solution.getComponents(nodeList, nums)
 
 print(result)
 
