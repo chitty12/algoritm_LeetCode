15e6f6fcd53e7037974b42e092dab155c6665bf6
chitty12 <cty12angel@naver.com>
2024-04-21 16:00:13 +0900

    linked_list components

--- linked_list.py
+++ linked_list.py
@@ -5,3 +5,56 @@
 # access(index): O(1)
 # insert(item, index): O(n)
 # remove(index): O(n)
+from typing import Optional
+
+
+class ListNode :
+    def __init__(self, val = 0, next = None) :
+        self.val = val
+        self.next = next
+    
+    def LinkedList (self, headList):
+        head = ListNode(headList[0])
+
+        for node in headList[1:]:
+            head.next = ListNode(node)
+            head = head.next
+        return head
+            
+
+
+class Solution :
+    # Optional : typing 모듈에서 제공하는 타입, None 또는 Listnode값을 가짐
+    def getComponents(self, head: Optional[ListNode], nums):
+        
+        count = 0
+        # for는 'iterable'을 순회하므로 적절하지 않음.
+        # while은 연속된 노드를 하나씩 순회하면서 확인함.
+        while head:
+            if head.val in nums:
+                if head.next and head.next in nums:
+                    count += 1
+            head = head.next
+
+
+        return count
+
+
+
+head = [0, 1, 2, 3]
+nums = [0, 1, 3]
+
+node_list = ListNode()
+solution = Solution()
+
+head = node_list.LinkedList(head)
+result = solution.getComponents(head, nums)
+
+print(result)
+
+
+                
+
+
+
+
