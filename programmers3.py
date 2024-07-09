233350766ab95f9967aa714d0e70a92372c0c59b
TaeYoung Choi <cty12angel@naver.com>
2024-07-09 21:55:46 +0900

    add refactoring code

--- programmers3.py
+++ programmers3.py
@@ -22,3 +22,14 @@ def solution(array, commands):
 ex_array = [1, 5, 2, 6, 3, 7, 4]
 ex_commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 print(solution(ex_array, ex_commands))
+
+
+
+def solution_refactoring(array, commands):
+    result = []
+    for command in commands:
+        i, j, k = command
+        sliced_array = array[i-1:j]
+        sliced_array.sort()
+        result.append(sliced_array[k-1])
+    return result
